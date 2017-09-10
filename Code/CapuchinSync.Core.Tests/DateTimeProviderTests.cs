2ee6f91e49fd9e91dcbc7a4068f28aae51762819
Pete Schallot <15661553+schallot@users.noreply.github.com>
2017-09-10 15:33:33 -0400

    More tests.

---
+++ Code/CapuchinSync.Core.Tests/DateTimeProviderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using CapuchinSync.Core;
+
+namespace CapuchinSync.Core.Tests
+{
+    [TestFixture]
+    public class DateTimeProviderTests
+    {
+        private DateTime _dateTime;
+        private DateTimeProvider _provider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dateTime = new DateTime(1999,1,2,3,4,5,6);
+            _provider = new DateTimeProvider();
+            ;
+        }
+
+        [Test]
+        public void GetTimeStringTest()
+        {
+            Assert.AreEqual("3:04:05 AM", _provider.GetTimeString(_dateTime));
+        }
+
+        [Test]
+        public void GetDateStringTest()
+        {
+            Assert.AreEqual("Saturday, January 2, 1999", _provider.GetDateString(_dateTime));
+        }
+
+        [Test]
+        public void NowTest()
+        {
+            var expectedNow = DateTime.Now;
+            var actual = _provider.Now;
+            var difference = expectedNow - actual;
+            int acceptableDifferenceInMilliseconds = 1000;
+            Assert.IsTrue(difference.TotalMilliseconds <= acceptableDifferenceInMilliseconds && difference.TotalMilliseconds >= -acceptableDifferenceInMilliseconds, 
+                $"Expected provider's Now property to match DateTime.Now's value within {acceptableDifferenceInMilliseconds} milliseconds, but was actually {difference.TotalMilliseconds}");
+        }
+    }
+}
---
+++ Code/CapuchinSync.Core.Tests/PathUtilityTests.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace CapuchinSync.Core.Tests
+{
+    [TestFixture]
+    public class PathUtilityTests
+    {
+        private PathUtility _pathUtility;
+        [SetUp]
+        public void SetUp()
+        {
+            _pathUtility = new PathUtility();
+        }
+
+        [Test]
+        public void GetParentDirectoryFromPathTest()
+        {
+            Assert.AreEqual("C:\\Temp", _pathUtility.GetParentDirectoryFromPath("C:\\Temp\\Blah"));
+        }
+
+        [Test]
+        public void CombineTest()
+        {
+            Assert.AreEqual("C:\\Temp\\Blah", _pathUtility.Combine("C:\\Temp\\","Blah"));
+        }
+
+        [Test]
+        public void GetTempFileNameTest()
+        {
+            var file = _pathUtility.GetTempFileName();
+            Assert.IsTrue(File.Exists(file),$"Expected temp file {file} to have already been created");
+            var info = new FileInfo(file);
+            Assert.AreEqual(0,info.Length, "Expected temp file to be zero bytes in length.");
+            File.Delete(file);
+        }
+
+        [Test]
+        public void GetFileNameTest()
+        {
+            Assert.AreEqual("blah",_pathUtility.GetFileName("C:\\Temp\\blah"));
+        }
+    }
+}
